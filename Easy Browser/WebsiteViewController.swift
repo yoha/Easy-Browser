77e4eb097e8c121f0bd617e9d666c1ec8367b50f
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-08-06 13:56:43 +0700

    Added: table view to present to user several websites to be loaded upon selection. Todo: app icon. Tofix: KVO operation is malfunctioning.

---
+++ Easy Browser/WebsiteViewController.swift
@@ -0,0 +1,89 @@
+//
+//  WebsiteViewController.swift
+//  Easy Browser
+//
+//  Created by Yohannes Wijaya on 8/4/15.
+//  Copyright © 2015 Yohannes Wijaya. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebsiteViewController: UIViewController, WKNavigationDelegate {
+    
+    // MARK: - Stored properties
+    
+    var webView: WKWebView!
+    var progressView: UIProgressView!
+    var website: String!
+    
+    // MARK: - Superclass methods override
+    
+    override func loadView() {
+        self.webView = WKWebView()
+        self.webView.navigationDelegate = self
+        self.view = self.webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Done, target: self, action: "returnToTableView")
+        self.progressView = UIProgressView(progressViewStyle: UIProgressViewStyle.Default)
+        self.progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: self.progressView)
+        let spacerBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: self, action: nil)
+        let refreshBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Refresh, target: self.webView, action: "reload" )
+        self.toolbarItems = [progressButton, spacerBarButtonItem, refreshBarButtonItem]
+        self.navigationController?.toolbarHidden = false
+        self.webView.addObserver(self, forKeyPath: "estimatedProgress", options: NSKeyValueObservingOptions.New, context: nil)
+        if let url = NSURL(string: "https://" + self.website) {
+            self.webView.loadRequest(NSURLRequest(URL: url))
+            self.webView.allowsBackForwardNavigationGestures = true
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [NSObject : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if keyPath == "estimatedProgress" { self.progressView.progress = Float(self.webView.estimatedProgress) }
+    }
+    
+     // MARK: - Delegate methods
+    
+    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+        self.title = self.webView.title!
+    }
+    
+    func webView(webView: WKWebView, decidePolicyForNavigationAction navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.URL!
+        if let host = url.host where host.rangeOfString(self.website) != nil {
+            decisionHandler(WKNavigationActionPolicy.Allow)
+            return
+        }
+        decisionHandler(WKNavigationActionPolicy.Cancel)
+    }
+
+    // MARK: - Local methods
+    
+    func returnToTableView() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    /*
+    func openTapped() {
+        let alertController = UIAlertController(title: "Open page...", message: "Tap the page you want to access", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        for site in self.websites {
+            alertController.addAction(UIAlertAction(title: site, style: UIAlertActionStyle.Default, handler: openPage))
+        }
+        alertController.addAction(UIAlertAction(title: "Cancel...", style: UIAlertActionStyle.Cancel, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    func openPage(alertAction: UIAlertAction) {
+        let url = NSURL(string: "https://" + alertAction.title!)
+        self.webView.loadRequest(NSURLRequest(URL: url!))
+    }
+    */
+}
---
+++ Easy Browser/WebsitesTableViewController.swift
@@ -0,0 +1,97 @@
+//
+//  WebsitesTableViewController.swift
+//  Easy Browser
+//
+//  Created by Yohannes Wijaya on 8/5/15.
+//  Copyright © 2015 Yohannes Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class WebsitesTableViewController: UITableViewController {
+    
+    // MARK: - Stored properties
+    
+    let websites = ["techmeme.com", "google.com", "duckduckgo.com"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.websites.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cellID", forIndexPath: indexPath)
+        let website = self.websites[indexPath.row]
+        cell.textLabel!.text = website
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "loadWebsite" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let website = self.websites[indexPath.row]
+                let websiteViewContoller = (segue.destinationViewController as! UINavigationController).topViewController as! WebsiteViewController
+                websiteViewContoller.website = website
+            }
+        }
+    }
+
+}
